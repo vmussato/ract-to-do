3243092b3de987260aa224b8d4ee0cf461b99c71
Vinicius Mussato <vinicius.mussato@gmail.com>
2022-09-28 12:43:58 -0300

    styling with css modules

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import Todo from './components/Todo';
 import InlineComponent from './components/InlineComponent';
 import StylesheetComponent from './components/StylesheetComponent';
+import ModuleComponent from './components/ModuleComponent';
 
 
 function App() {
   return (
     <div className="App">
-      <StylesheetComponent />
+      <ModuleComponent />
       {/* <InlineComponent />
       <Todo /> */}
     </div>
---
+++ src/components/ModuleComponent.js
@@ -0,0 +1,5 @@
+import styles from "./style.module.css";
+
+export default function ModuleComponent() {
+    return <div className={styles.header}>this is a module component</div>
+}
