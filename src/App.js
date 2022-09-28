13a50480bad822fb34b7d3d4daa881b35f238618
Vinicius Mussato <vinicius.mussato@gmail.com>
2022-09-28 12:38:41 -0300

    Regular styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Todo from './components/Todo';
 import InlineComponent from './components/InlineComponent';
+import StylesheetComponent from './components/StylesheetComponent';
 
 
 function App() {
   return (
     <div className="App">
-      <InlineComponent />
-      <Todo />
+      <StylesheetComponent />
+      {/* <InlineComponent />
+      <Todo /> */}
     </div>
   );
 }
---
+++ src/components/StylesheetComponent.js
@@ -0,0 +1,5 @@
+import "./style.css";
+
+export default function StylesheetComponent() {
+    return <div className="header">This is a stylesheet component</div>
+}
